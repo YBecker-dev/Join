6f233e9302086ee3d57c7016380167db3e6fe542
jthiel <lthiel@dsoftworks.de>
2025-06-15 00:53:31 +0200

    Overlay created Additional icons added Contacts added via render function

--- assets/script/contacts.js
+++ assets/script/contacts.js
@@ -1,1 +1,41 @@
-let myContacts = [];
+let myContacts = [
+   {
+   "givenName": "Gabi",
+   "surname": "Schuster",
+   "mail": "gabischuster@gmail.com",
+   "phone": "+49123456789"
+   },
+   {
+    "givenName": "Holger",
+   "surname": "Arnold",
+   "mail": "holgerarnold@gmail.com",
+   "phone": "+49123455690"
+   },
+     {
+    "givenName": "Michaela",
+   "surname": "Klein",
+   "mail": "michaelaklein@gmail.com",
+   "phone": "+49123455681"
+   },  
+];
+
+function init() {
+    renderContacts();
+    renderContactsOverlay();
+}
+
+function renderContacts() {
+    let myContactsContentRef = document.getElementById('contactContent');
+
+    for (let i = 0; i < myContacts.length; i++) {
+        myContactsContentRef.innerHTML += getNoteTemplateContact(i);
+    }
+}
+
+function getInitials(first, last) {
+    return first[0].toUpperCase() + last[0].toUpperCase();
+}
+
+function openContactOverlay() {
+    
+}
--- assets/script/templates.js
+++ assets/script/templates.js
@@ -1,12 +1,39 @@
-
-function getNoteTemplateContact() {
-    return `
-            <div onclick="openContactOverlay()" class="person">
-                <p class="initial">GS</p>
+function getNoteTemplateContact(index) {
+    let contact = myContacts[index];
+    
+    return `<div onclick="openContactOverlay()" class="person">
+                <p class="initial">${getInitials(contact.givenName, contact.surname)}</p>
                 <div>
-                    <h4>${givenName} ${surname}</h4>
-                    <p><a class="mail" href="mailto:">${mail}</a></p>
+                    <h4>${contact.givenName} ${contact.surname}</h4>
+                    <p><a class="mail">${contact.mail}</a></p>
                 </div>
-            </div>
-        `
-};
+            </div>`;
+}
+
+function getNoteTemplateContactOverlay() {
+
+    return `<div class="contactOverlay">
+                    <div class="myNames">
+                        <p class="initialOverlay">${getInitials(contact.givenName, contact.surname)}</p>
+                        <div>
+                            <h3>${contact.givenName} ${contact.surname}</h3>
+                            <div class="contactIcons">
+                                <div onclick="openContactOverlay()">
+                                    <img class="editIcon" src="../img/icon/edit.png" alt="pencil">
+                                    <span>edit</span>
+                                </div>
+                                <div onclick="deleteContact()">
+                                    <img class="editIcon" src="../img/icon/trash.png" alt="wastebasket">
+                                    <span>delete</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p>Contact Information</p>
+                    <p><strong>Email</strong></p>
+                    <p><a class="mail" href="mailto:">${contact.mail}</a></p>
+                    <p><strong>Phone</strong></p>
+                    <p><a class="phone" href="tel:">${contact.phone}</a></p>
+                </div>
+                        `
+}
