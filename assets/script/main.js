0a6e8f0b5c9b9541fe503dd615bc862a33d3c893
Yannick Becker <beckeryannick18@gmail.com>
2025-06-15 13:52:09 +0200

    html changes

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,10 +8,11 @@ function loadContent(page) {
         initAddTask();
       } else if (page === 'contacts.html') {
       } else if (page === 'board.html') {
-      } else if (page === 'summary.html') {
+      } else if (page === 'summary_user.html') {
         initSummary();
       } else if (page === 'privacy-policy.html') {
       } else if (page === 'legal-notice.html') {
       }
     });
 }
+  
