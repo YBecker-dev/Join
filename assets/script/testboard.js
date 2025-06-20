9bbb7d03c0037682382c6a5c5e28375ba7139860
Yannick Becker <beckeryannick18@gmail.com>
2025-06-20 17:09:10 +0200

    Css changes in testboard.css, now you can create a task and the data comes to testboard.html

--- assets/script/testboard.js
+++ assets/script/testboard.js
@@ -14,44 +14,77 @@ async function pushTasksInBoard() {
   for (let i = 0; i < entries.length; i++) {
     let taskId = entries[i][0];
     let task = entries[i][1];
+
     let categoryText = '';
+    let categoryClass = '';
     if (task.category) {
       categoryText = task.category;
-    }
-    let titleText = '';
-    if (task.title) {
-      titleText = task.title;
-    }
-    let descriptionText = '';
-    if (task.description) {
-      descriptionText = task.description;
-    }
-    let subtasksText = '';
-    if (Array.isArray(task.subtasks)) {
-      for (let j = 0; j < task.subtasks.length; j++) {
-        subtasksText += '- ' + task.subtasks[j] + '<br>';
+      if (task.category === 'User Story') {
+        categoryClass = 'category-user-story';
+      } else if (task.category === 'Technical Task') {
+        categoryClass = 'category-technical-task';
       }
     }
-    let assignedText = '';
+
+    let titleText = '';
+    if (task.title) titleText = task.title;
+
+    let descriptionText = '';
+    if (task.description) descriptionText = task.description;
+
+    let assignedContact = '';
     if (Array.isArray(task.assignedTo)) {
-      assignedText = task.assignedTo.join(', ');
+      for (let contactIndex = 0; contactIndex < task.assignedTo.length; contactIndex++) {
+        let name = task.assignedTo[contactIndex];
+        assignedContact += `<span class="board-contact-name">${name}</span>`;
+      }
     }
-    let priorityText = '';
-    if (task.priority) {
-      priorityText = task.priority;
+
+    let priorityImg = '';
+    if (task.priority === 'Urgent') {
+      priorityImg = '<img src="../img/icon/priority/urgent.png" alt="Urgent" class="priority-img">';
+    } else if (task.priority === 'Medium') {
+      priorityImg = '<img src="../img/icon/priority/medium.png" alt="Medium" class="priority-img">';
+    } else if (task.priority === 'Low') {
+      priorityImg = '<img src="../img/icon/priority/low.png" alt="Low" class="priority-img">';
     }
+
+let doneCount = 0;
+let totalCount = 0;
+let progressBar = '';
+if (Array.isArray(task.subtasks)) {
+  totalCount = task.subtasks.length;
+  for (let subtaskIndex = 0; subtaskIndex < task.subtasks.length; subtaskIndex++) {
+    if (typeof task.subtasks[subtaskIndex] === 'object' && task.subtasks[subtaskIndex].done) {
+      doneCount++;
+    }
+  }
+  let percent = totalCount > 0 ? (doneCount / totalCount) * 100 : 0;
+  progressBar = `
+    <div class="board-task-subtasks-row">
+      <div class="progress-bar-container">
+        <div class="progress-bar" style="width: ${percent}%;"></div>
+      </div>
+      <span class="progress-bar-text">${doneCount}/${totalCount} Subtasks</span>
+    </div>
+  `;
+    }
+
     let div = document.createElement('div');
     div.className = 'board-task-container';
     div.innerHTML = `
       <div class="board-tasks">
-        <p class="board-task-category">${categoryText}</p>
+    <p class="${categoryClass}">${categoryText}</p>
         <div class="board-tasks-title-description">
           <p class="board-task-title">${titleText}</p>
           <p class="board-task-description">${descriptionText}</p>
         </div>
-        <p class="board-task-subtasks">${subtasksText}</p>
-        <p class="board-task-assigned-priority">${assignedText} / ${priorityText}</p>
-        <button onclick="deleteTaskFromFirebase('${taskId}')">delete</button>
+        <div class="board-task-subtasks">${progressBar}</div>
+        <div class="board-task-assigned-priority">
+          <div class="board-task-assigned-contact">${assignedContact}</div>
+          ${priorityImg}
+        </div>
+        <button onclick="deleteTaskFromFirebase('${taskId}')">test delete</button>
       </div>
     `;
     section.appendChild(div);
