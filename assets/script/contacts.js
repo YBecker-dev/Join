cda578e63fadad8040ccf098af32757f696dfd8c
jthiel <jthiel@dsoftworks.de>
2025-06-26 18:44:44 +0200

    Delet function declared

--- assets/script/contacts.js
+++ assets/script/contacts.js
@@ -77,14 +77,17 @@ function openEditOverlay() {
     setEvantlistlener = 'click'
 }
 
+function deleteContact() {
+
+}
+
 
 function toggleContactOverlay() {
     console.log('connect')
     let overlayRef = document.getElementById('overlayContact');
-    //let overlay_content = document.getElementById('overlay-contact-content-loader');
+    let overlay_content = document.getElementById('overlay-contact-content-loader');
     overlayRef.classList.toggle('d-none');
     if(!overlayRef.classList.contains('d-none')){
-        overlay_content.innerHTML= getAddContactOverlay();
+        overlay_content.innerHTML= getNoteTemplateAddNewContact();
     }   
 }
-
