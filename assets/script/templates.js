818ec51b1329b9acb0e308ebd32be6c29d2cfd33
raphael <raphael.zwick8@gmail.com>
2025-07-15 12:23:14 +0200

    update responsiv design at Privacy-policy and legal-notice

--- assets/script/templates.js
+++ assets/script/templates.js
@@ -220,13 +220,13 @@ function overlaySubtaskHtml(subtask, subtaskIndex, taskId) {
 
 function getLogOutMenu() {
   return `
-      <div class="help-mobil" onclick="loadContent('help.html')" id="help-mobil">
+      <div class="help-mobil" onclick="window.location.href='/assets/html/MPA-architecture/help.html'" id="help-mobil">
           <p>Help</p>
       </div>
-      <div class="section1" onclick="loadContent('legal-notice.html')"  id="legalNotice">
+      <div class="section1" onclick="window.location.href='/assets/html/MPA-architecture/legal-notice_MPA.html'"  id="legalNotice">
           <p> Legal Notice</p>
       </div>
-      <div class="section1" onclick="loadContent('privacy-policy.html')"  id="privacyPolicy">
+      <div class="section1" onclick="window.location.href='/assets/html/MPA-architecture/privacy-policy_MPA.html'"  id="privacyPolicy">
           <p>Privacy Policy</p>
       </div>
       <div class="section1" onclick="handleLogOut(event)"  id="logOut">
